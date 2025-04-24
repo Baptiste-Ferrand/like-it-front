72bf33128f17c36d14fa72955c99f5647780deec
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2025-04-24 22:31:14 +0200

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from './router/AppRouter';
-import './index.css'; // styles Tailwind
-import { attachTokenToAxios } from './utils/auth/token';
-
-attachTokenToAxios();
+import { AuthProvider } from './context/AuthContext';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
-      <AppRouter />
-    </div>
-  </React.StrictMode>,
+    <AuthProvider>
+      <div className="min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+        <AppRouter />
+      </div>
+    </AuthProvider>
+  </React.StrictMode>
 );
